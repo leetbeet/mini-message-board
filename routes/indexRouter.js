a9ae2a98fb1e98b265911e9b55c2854510882af3
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-09 11:21:50 +0800

    feat: add link to message page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -14,21 +16,30 @@ const messages = [
 ];
 
 const indexRouter = Router();
+let nextId = 2;
 
-indexRouter.get("/", (req, res) =>
-  res.render("index", { title: "Mini Messageboard", messages: messages }),
-);
+indexRouter.get("/", (req, res) => res.render("index", { messages }));
 indexRouter.get("/new", (req, res) => res.render("form"));
 indexRouter.post("/new", (req, res) => {
   const { message, author } = req.body;
 
   messages.push({
+    id: ++nextId,
     text: message,
     user: author,
     added: new Date(),
   });
 
   res.redirect("/");
+});
+indexRouter.get("/:id", (req, res) => {
+  const message = messages.find((m) => m.id === Number(req.params.id));
+
+  if (!message) {
+    return res.status(404).send("Message not found");
+  }
+
+  res.render("message", { message });
 });
 
 module.exports = indexRouter;
