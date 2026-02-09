d504684b273f5b7d10a04428cebe0515d970064b
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-09 08:28:07 +0800

    feat: add app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`My first Express app - listening on port ${PORT}!`);
+});
