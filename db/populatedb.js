d579cc9c67bcae001edc1d49009ec40e5fd4b61e
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-18 12:02:32 +0800

    feat: setup database

---
+++ db/populatedb.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  text VARCHAR ( 255 ),
+  user VARCHAR ( 255 ),
+  added DATE
+);
+
+INSERT INTO messages (text, user, added) 
+VALUES
+  ('Hi there!', 'Amando', Now()),
+  ('Hello World!', 'Charles', Now());
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DB_URL,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
