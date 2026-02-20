3a71d2a92fa5bfa8e36bb3654941108fbce6efb5
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-20 09:52:10 +0800

    feat: add server side form validation

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,26 +1,35 @@
 const { Router } = require("express");
-
-const messages = [
-  {
-    id: 1,
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    id: 2,
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
+const { body, validationResult } = require("express-validator");
 const indexRouter = Router();
-let nextId = 2;
+
+const alphaErr = "must only contain letters.";
+
+const validateUser = [
+  body("author")
+    .trim()
+    .isAlpha()
+    .withMessage(`author ${alphaErr}`)
+    .isLength({ min: 1, max: 20 })
+    .withMessage("author must be between 1 and 20 characters"),
+  body("message")
+    .trim()
+    .isAlpha()
+    .withMessage(`message ${alphaErr}`)
+    .isLength({ min: 1, max: 200 })
+    .withMessage("Message must be between 1 and 200 characters lonh"),
+];
 
 indexRouter.get("/", (req, res) => res.render("index", { messages }));
 indexRouter.get("/new", (req, res) => res.render("form"));
-indexRouter.post("/new", (req, res) => {
+indexRouter.post("/new", validateUser, (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).render("form", {
+      errors: errors.array(),
+    });
+  }
+
   const { message, author } = req.body;
 
   messages.push({
