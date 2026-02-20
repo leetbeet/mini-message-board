589174c745bd7f869cc6feb06ea13b3603584ae7
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-20 09:37:52 +0800

    feat: add functions to query database

---
+++ db/queries.js
@@ -0,0 +1,17 @@
+const pool = require("./pool");
+
+async function addMessage(author, message) {
+  const query =
+    "INSERT INTO messages (text, user, added) VALUES ($1, $2, NOW())";
+  const values = [message, author];
+  await pool.query(query, values);
+}
+
+async function findMessage(id) {
+  return await pool.query("SELECT * FROM messages WHERE id = $1", [id]);
+}
+
+module.exports = {
+  addMessage,
+  findMessage,
+};
