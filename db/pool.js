29060f83feeb098beb4546ee3ef4446afe340b8d
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-20 11:10:17 +0800

    fix: pool

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,5 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB,
-  password: process.env.DB_PASSWORD,
-  port: process.env.PORT,
+  connectionString: process.env.DB_URL,
 });
